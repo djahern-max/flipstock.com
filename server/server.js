2c7196eb26993d01ba751327bbdfac446be5ac22
djahern-max <danielaherniv@gmail.com>
2023-05-14 09:52:17 -0400

    Commit

--- server/server.js
+++ server/server.js
@@ -16,11 +16,14 @@ app.use('/api/blogs', blogsRoute)
 app.use('/api/blog-actions', blogActionsRoute)
 
 //Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging'
+) {
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
   })
 }
 
